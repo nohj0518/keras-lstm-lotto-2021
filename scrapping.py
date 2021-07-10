3b9dfcb3b0eeb996ba5edf85993a51a6730e088c
nohj0518 <38433755+nohj0518@users.noreply.github.com>
2021-07-11 00:56:13 +0900

    Create scrapping.py

---
+++ scrapping.py
@@ -0,0 +1,98 @@
+import requests
+from bs4 import BeautifulSoup
+
+main_url = "https://www.dhlottery.co.kr/gameResult.do?method=byWin"  # 마지막 회차를 얻기 위한 주소
+basic_url = (
+    "https://www.dhlottery.co.kr/gameResult.do?method=byWin&drwNo="  # 임의의 회차를 얻기 위한 주소
+)
+
+# 마지막 회차 정보를 가져옴
+def GetLast():
+    resp = requests.get(main_url)
+    soup = BeautifulSoup(resp.text, "lxml")
+    result = str(soup.find("meta", {"id": "desc", "name": "description"})["content"])
+    s_idx = result.find(" ")
+    e_idx = result.find("회")
+    return int(result[s_idx + 1 : e_idx])
+
+
+# 지정된 파일에 지정된 범위의 회차 정보를 기록함
+def Crawler(s_count, e_count, fp):
+    for i in range(s_count, e_count + 1):
+        crawler_url = basic_url + str(i)
+        resp = requests.get(crawler_url)
+        soup = BeautifulSoup(resp.text, "html.parser")
+
+        text = soup.text
+
+        s_idx = text.find(" 당첨결과")
+        s_idx = text.find("당첨번호", s_idx) + 4
+        e_idx = text.find("보너스", s_idx)
+        numbers = text[s_idx:e_idx].strip().split()
+
+        s_idx = e_idx + 3
+        e_idx = s_idx + 3
+        bonus = text[s_idx:e_idx].strip()
+
+        s_idx = text.find("1등", e_idx) + 2
+        e_idx = text.find("원", s_idx) + 1
+        e_idx = text.find("원", e_idx)
+        money1 = text[s_idx:e_idx].strip().replace(",", "").split()[2]
+
+        s_idx = text.find("2등", e_idx) + 2
+        e_idx = text.find("원", s_idx) + 1
+        e_idx = text.find("원", e_idx)
+        money2 = text[s_idx:e_idx].strip().replace(",", "").split()[2]
+
+        s_idx = text.find("3등", e_idx) + 2
+        e_idx = text.find("원", s_idx) + 1
+        e_idx = text.find("원", e_idx)
+        money3 = text[s_idx:e_idx].strip().replace(",", "").split()[2]
+
+        s_idx = text.find("4등", e_idx) + 2
+        e_idx = text.find("원", s_idx) + 1
+        e_idx = text.find("원", e_idx)
+        money4 = text[s_idx:e_idx].strip().replace(",", "").split()[2]
+
+        s_idx = text.find("5등", e_idx) + 2
+        e_idx = text.find("원", s_idx) + 1
+        e_idx = text.find("원", e_idx)
+        money5 = text[s_idx:e_idx].strip().replace(",", "").split()[2]
+
+        line = (
+            str(i)
+            + ","
+            + numbers[0]
+            + ","
+            + numbers[1]
+            + ","
+            + numbers[2]
+            + ","
+            + numbers[3]
+            + ","
+            + numbers[4]
+            + ","
+            + numbers[5]
+            + ","
+            + bonus
+            + ","
+            + money1
+            + ","
+            + money2
+            + ","
+            + money3
+            + ","
+            + money4
+            + ","
+            + money5
+        )
+        print(line)
+        line += "\n"
+        fp.write(line)
+
+
+last = GetLast()  # 마지막 회차를 가져옴
+
+fp = open("keras_lstm_lotto_v895_data.csv", "w")
+Crawler(1, last, fp)  # 처음부터 마지막 회차까지 저장
+fp.close()
